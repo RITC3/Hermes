70143dbdfb1277f1a9924e92acdce1d3c701a45f
nmpiazza <nmp5860@rit.edu>
2016-06-26 23:05:17 -0400

    Finish migration of functions from api-serv.py to endpoints.py

--- Hermes/Server/api-serv.py
+++ Hermes/Server/api-serv.py
@@ -3,82 +3,45 @@ from flask import Flask, jsonify
 #TODO Implement TLS
 
 app = Flask(__name__)
+app.run()
 
 @app.errorhandler(404)
 def page_not_found(error):
     """Return generic 404 page"""
     return 'Requested page does not exist', 404
 
-#TODO Move the function definitions to the another file called api-handlers.py
+@app.route('/')
+def welcome():
+    return 'Welcome to the Hermes Scoring server'
 
-#Client API endpoint list
-@app.route('/client/session/open')
-def ClientSessionOpen():
-    """
-    Creates a session for a scoring bot
-    """
-    pass
+#Bot API endpoint list
+@app.route('/bot/session/open')
+def FlaskBotSessionOpen():
+    endpoints.BotSessionOpen()
 
-@app.route('/client/session/close')
-def ClientSessionClose():
-    """
-    Requires an active client session
-    Requires a valid client session cookie
-    This will deactivate the session for that client
-    """
-    pass
+@app.route('/bot/session/close')
+def FlaskBotSessionClose():
+    endpoints.BotSessionClose()
 
-@app.route('/client/session/update')
-def ClientSessionUpdate():
-    #TODO Will the client supply other data as well
-    """
-    Requires an active client session
-    Requires a valid client session cookie
-    The scoring bot will provide the number of available subprocesses
-        available for checks
-    The scoring bot will provide the maximum number of checks it supports
-    """
-    pass
+@app.route('/bot/session/update')
+def FlaskBotSessionUpdate():
+    endpoints.BotSessionUpdate()
 
-@app.route('/client/jobs/list')
-def ClientJobsList():
-    """
-    Requires an active client session
-    Requires a valid client session cookie
-    Lists all of the checks that are queued
-    """
-    pass
+@app.route('/bot/jobs/list')
+def FlaskBotJobsList():
+    endpoints.BotJobsList()
 
-@app.route('/client/jobs/check-in')
-def ClientJobsCheckin():
-    """
-    Requires an active client session
-    Requires a valid client session cookie
-    Client will send the status of each currently active job
-    Client will send the status of all recently completed job
-    """
-    pass
+@app.route('/bot/jobs/check-in')
+def FlaskBotJobsCheckin():
+    endpoints.BotJobsCheckin()
 
-@app.route('/client/jobs/request')
-def ClientJobsRequest():
-    """
-    Requires an active client session
-    Requires a valid client session cookie
-    Client requests one job to take on
-    Server responds with a job for the client to execute
-    Server places that job into an "Active" list
-    """
-    pass
+@app.route('/bot/jobs/request')
+def FlaskBotJobsRequest():
+    endpoints.BotJobsRequest()
 
-@app.route('/client/jobs/abort')
-def ClientJobsAbort():
-    """
-    Requires an active client session
-    Requires a valid client session cookie
-    Client notifies the server that the job has been aborted
-    Server places that job back into the available pool
-    """
-    pass
+@app.route('/bot/jobs/abort')
+def FlaskBotJobsAbort():
+    endpoints.BotJobsAbort()
 
 #TODO Finish prototyping server API endpoints
 #TODO Add requirements for server API endpoints
@@ -88,65 +51,65 @@ If it is referencing a host on a team, it will need
 to send a JSON object specifying those parameters
 """
 @app.route('/web-gui/session/open')
-def WebGuiSessionOpen():
-    pass
+def FlaskWebServerSessionOpen():
+    endpoints.WebServerSessionOpen()
 
 @app.route('/web-gui/session/close')
-def WebGuiSessionClose():
-    pass
+def FlaskWebServerSessionClose():
+    endpoints.WebServerSessionClose()
 
 @app.route('/web-gui/team/list')
-def WebGuiTeamList():
-    pass
+def FlaskWebServerTeamList():
+    endpoints.WebServerTeamList()
 
 @app.route('/web-gui/team/create')
-def WebGuiTeamCreate():
-    pass
+def FlaskWebServerTeamCreate():
+    endpoints.WebServerTeamCreate()
 
 @app.route('/web-gui/team/delete')
-def WebGuiTeamDelete():
-    pass
+def FlaskWebServerTeamDelete():
+    endpoints.WebServerTeamDelete()
 
 @app.route('/web-gui/host/list')
-def WebGuiHostList():
-    pass
+def FlaskWebServerHostList():
+    endpoints.WebServerHostList()
 
 @app.route('/web-gui/host/create')
-def WebGuiHostCreate():
-    pass
+def FlaskWebServerHostCreate():
+    endpoints.WebServerTeamCreate()
 
 @app.route('/web-gui/host/delete')
-def WebGuiHostDelete():
-    pass
+def FlaskWebServerHostDelete():
+    endpoints.WebServerHostDelete()
 
 @app.route('/web-gui/service/list')
-def WebGuisServiceList():
-    pass
+def FlaskWebServersServiceList():
+    endpoints.WebServersServiceList()
 
 @app.route('/web-gui/service/create')
-def WebGuiServiceCreate():
-    pass
+def FlaskWebServerServiceCreate():
+    endpoints.WebServerServiceCreate()
 
 @app.route('/web-gui/service/delete')
-def WebGuiServiceDelete():
-    pass
+def FlaskWebServerServiceDelete():
+    endpoints.WebServerServiceDelete()
 
 @app.route('/web-gui/check/enable')
-def WebGuiCheckEnable():
-    pass
+def FlaskWebServerCheckEnable():
+    endpoints.FlaskWebServerCheckEnable()
 
 @app.route('/web-gui/check/disable')
-def WebGuiCheckDisable():
-    pass
+def FlaskWebServerCheckDisable():
+    endpoints.WebServerCheckDisable()
 
 @app.route('/web-gui/check/interval/list')
-def WebGuiCheckIntervalList():
-    pass
+def FlaskWebServerCheckIntervalList():
+    endpoints.WebServerCheckIntervalList()
 
 @app.route('/web-gui/check/interval/set')
-def WebGuiCheckIntervalSet():
-    pass
+def FlaskWebServerCheckIntervalSet():
+    endpoints.WebServerCheckIntervalSet()
 
 @app.route('/web-gui/check/interval/default')
-def WebGuiCheckIntervalDefault():
-    pass
+def FlaskWebServerCheckIntervalDefault():
+    endpoints.WebServerCheckIntervalDefault()
