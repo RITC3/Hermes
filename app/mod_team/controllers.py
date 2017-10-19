abb543e7376b3972086f9facf98cb084bad6a06f
Joel Margolis <joel@silocode.net>
2017-10-19 15:37:25 -0400

    Add updating teams to API

--- app/mod_team/controllers.py
+++ app/mod_team/controllers.py
@@ -10,10 +10,9 @@ mod_team = Blueprint('team', __name__, url_prefix='/api/data/team')
 @mod_team.route('/create', methods=['POST'])
 @require_auth
 def create_team():
-    # get the 'name' field from the POST form data
+    # if the team name was provided
     name = request.form.get('name')
     if name:
-        # if the name was sent
         try:
             team = Team(name=name)
             db.session.add(team)
@@ -29,17 +28,48 @@ def create_team():
 @mod_team.route('/remove', methods=['POST'])
 @require_auth
 def remove_team():
+    # if the team ID was provided
     team_id = request.form.get('id')
     if team_id:
         try:
-            team = Team.query.filter_by(id=team_id).first()
+            team = Team.query.filter(Team.id == team_id).first()
             if team is not None:
                 db.session.delete(team)
                 db.session.commit()
-                
+
                 return jsonify({'success': True}), 200
         except SQLAlchemyError:
             db.session.rollback()
+
+    return jsonify({'success': False}), 400
+
+
+@mod_team.route('/update', methods=['POST'])
+@require_auth
+def update_team():
+    # check if team ID and possible properties were given
+    team_id = request.form.get('id')
+    available_keys = [request.form.get(k) for k in ['name', 'score']]
+    if team_id and any(available_keys):
+        name, score = available_keys
+
+        # check that the team exists
+        team = Team.query.filter(Team.id == team_id).first()
+        if team is not None:
+            # update the team name if provided
+            if name is not None:
+                team.name = name
+
+            # update the score if provided
+            if score is not None:
+                # catch the exception for is score isn't a valid int
+                try:
+                    team.score = int(score)
+                except ValueError:
+                    pass
+
+            db.session.commit()
+            return jsonify({'success': True}), 200
 
     return jsonify({'success': False}), 400
 
