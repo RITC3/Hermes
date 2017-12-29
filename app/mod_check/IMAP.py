45e79bfe9cbf20583fc4c9560c69d3128675f34e
Joel Margolis <joel@silocode.net>
2017-12-29 17:47:56 -0500

    Update IMAP module to read from Scoring mailbox (?)

--- app/mod_check/IMAP.py
+++ app/mod_check/IMAP.py
@@ -1,19 +1,8 @@
 from ..mod_check import app
 import imaplib
-import email
 import logging
 
 logger = logging.getLogger('mod_check.IMAP')
-
-
-def get_first_text_block(email_message_instance):
-    maintype = email_message_instance.get_content_maintype()
-    if maintype == 'multipart':
-        for part in email_message_instance.get_payload():
-            if part.get_content_maintype() == 'text':
-                return part.get_payload()
-    elif maintype == 'text':
-        return email_message_instance.get_payload()
 
 
 @app.task
@@ -31,16 +20,15 @@ def check(host, port, username, password, use_ssl=False):
 
         with connection as mail:
             mail.login(user=username, password=password)
-            mail.select('INBOX')
+            res, _ = mail.select('Scoring')
+            if res == 'OK':
+                result, data = mail.uid('search', None, 'ALL')
+                latest_email_uid = data[0].split()[-1]
 
-            result, data = mail.uid('search', None, 'ALL')
-            latest_email_uid = data[0].split()[-1]
-
-            result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
-            raw_email = data[0][1]
-            email_message = email.message_from_bytes(raw_email)
-            result = get_first_text_block(email_message)
-
+                result, data = mail.uid('fetch', latest_email_uid, '(RFC822.TEXT)')
+                result = data[0][1].decode('utf8')
+            else:
+                result = False
     except (imaplib.IMAP4_SSL.error, imaplib.IMAP4.error) as e:
         logger.exception(e)
 
