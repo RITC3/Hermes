2f54a56e0c61df4355be2b942a75af5406628c2a
Joel Margolis <joel@silocode.net>
2017-12-31 15:02:37 -0500

    Create SMTP check and update docker compose mail config

--- run.py
+++ run.py
@@ -7,16 +7,16 @@ from os import path
 import ssl
 
 
-def parse_and_run(args=None):
+def main():
     ssl_base_dir = path.join(BASE_DIR, 'ssl')
 
-    p = ArgumentParser()
-    p.add_argument('--bind', '-b', action='store', help='the address to bind to', default='127.0.0.1')
-    p.add_argument('--port', '-p', action='store', type=int, help='the port to listen on', default=12321)
-    p.add_argument('--debug', '-d', action='store_true', help='enable debugging (use with caution)', default=False)
-    p.add_argument('--ssl', '-s', action='store_true', help='enable ssl', default=False)
+    parser = ArgumentParser()
+    parser.add_argument('--bind', '-b', action='store', help='the address to bind to', default='127.0.0.1')
+    parser.add_argument('--port', '-p', action='store', type=int, help='the port to listen on', default=12321)
+    parser.add_argument('--debug', '-d', action='store_true', help='enable debugging (use with caution)', default=False)
+    parser.add_argument('--ssl', '-s', action='store_true', help='enable ssl', default=False)
 
-    args = p.parse_args(args)
+    args = parser.parse_args()
     if args.ssl:
         ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
         ctx.load_cert_chain(path.join(ssl_base_dir, 'server.crt'), path.join(ssl_base_dir, 'server.key'))
@@ -28,4 +28,4 @@ def parse_and_run(args=None):
 
 
 if __name__ == '__main__':
-    parse_and_run()
+    main()
