692d10b7032956bc6e4fb720b67d3619c0d32ec9
Joel Margolis <joel@silocode.net>
2017-12-31 12:44:56 -0500

    Finish IMAP check

--- app/mod_check/IMAP.py
+++ app/mod_check/IMAP.py
@@ -20,16 +20,47 @@ def check(host, port, username, password, use_ssl=False):
 
         with connection as mail:
             mail.login(user=username, password=password)
-            res, _ = mail.select('Scoring')
-            if res == 'OK':
-                result, data = mail.uid('search', None, 'ALL')
-                latest_email_uid = data[0].split()[-1]
 
-                result, data = mail.uid('fetch', latest_email_uid, '(RFC822.TEXT)')
-                result = data[0][1].decode('utf8')
+            scoring_exists = False
+            # check if the scoring mailbox exists
+            mb_res, _ = mail.select('Scoring')
+            if mb_res == 'OK':
+                # if it does, mark it for later use
+                scoring_exists = True
             else:
-                result = False
+                # if the scoring mailbox doesn't exist, select the inbox
+                mb_res, _ = mail.select('INBOX')
+
+            # if the result was OK (for either scoring or inbox)
+            if mb_res == 'OK':
+                # retrieve the ScoringCheck email
+                search_res, search_data = mail.uid('search', None, 'SUBJECT', 'ScoringCheck-')
+                if search_res == 'OK' and len(search_data) > 0:
+                    # split the email UIDs and check for the
+                    email_uids = search_data[0].split()
+                    if len(email_uids) > 0:
+                        latest_email_uid = email_uids[-1]
+
+                        result, data = mail.uid('fetch', latest_email_uid, '(RFC822.TEXT)')
+                        result = data[0][1].decode('utf8')
+
+                        if not scoring_exists:
+                            res, _ = mail.create('Scoring')
+                            if res == 'OK':
+                                res, _ = mail.copy(latest_email_uid, 'Scoring')
+                                if res != 'OK':
+                                    logging.error('Error copying email to Scoring mailbox')
+                            else:
+                                logging.error('Error creating Scoring mailbox')
+                    else:
+                        logging.error('No messages fetched')
+                else:
+                    logging.error('Error getting all messages')
+            else:
+                logger.error('Scoring mailbox does not exist')
+
     except (imaplib.IMAP4_SSL.error, imaplib.IMAP4.error) as e:
         logger.exception(e)
+        result = False
 
     return result
