c6d79195d6cbe3afeebd30b63284e8c8a9bfd628
Ohan <ohan@mail.rit.edu>
2018-01-03 01:39:00 -0500

    most of the skeleton is done, will need endpoints soon  to be able to continue

--- app/mod_webui/controllers.py
+++ app/mod_webui/controllers.py
@@ -8,11 +8,24 @@ from flask import Blueprint, request, render_template, make_response, redirect, 
 
 mod_webui = Blueprint('webui', __name__, url_prefix='/', template_folder='templates')
 
+@mod_webui.context_processor
+def all_services():
+    """
+        this allows for the services to be accessed from every template without copy/pasting
+    """
+    return dict(all_services=[('Web', 'wb_cloudy'), ('IMAP', 'drafts'), ('SMTP', 'email'), \
+                             ('LDAP', 'supervisor_account'), ('WinRM', 'contacts')])
 
 @mod_webui.route('/')
 def index():
     # show should be 2 if logged in (this logic might be bad)
-    return render_template('index.html', username=request.cookies.get('username'))
+    return render_template('index.html')
+
+# test route for admin functionality
+@mod_webui.route('admin')
+def admin():
+    # just for testing
+    return render_template('index.html', is_admin=True)
 
 # we leave off the leading / since it is declared as the url_prefix
 @mod_webui.route('register', methods=['GET', 'POST'])
@@ -22,7 +35,7 @@ def register():
         # do things with the stuff, aka create account and redirect
         val = ('not yet', 400)
     else:
-        val = render_template('register.html', username=True)
+        val = render_template('register.html')
     resp = make_response(val)
     resp.set_cookie('username', '', expires=0) 
     return resp
@@ -39,3 +52,15 @@ def logout():
     resp = make_response(redirect(url_for('webui.index')))
     resp.set_cookie('username', '', expires=0)
     return resp
+
+@mod_webui.route('config/<service>', methods=['GET', 'POST'])
+def config(service):
+    if request.method == 'POST':
+        # configure required service
+        return f"soon configure {service}"
+    else:
+        return render_template('configure.html', service=service)
+
+@mod_webui.route('results')
+def results():
+    return render_template('results.html')
